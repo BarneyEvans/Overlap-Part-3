9842f78bcc662af4365106db60cb75d012440b95
barney <evansba2701@gmail.com>
2024-03-04 13:53:29 +0000

    Working on Stereo Vision

--- Main.py
+++ Main.py
@@ -2,6 +2,8 @@ import numpy as np
 import cv2 as cv
 import glob
 from Cameras import Camera, camera5, camera6, camera7, camera8
+import os
+from matplotlib import pyplot as plt
 from PIL import Image
 
 image_paths = glob.glob('cam0[5-8].jpg')
@@ -18,8 +20,49 @@ for path in image_paths:
 
 
 def correct_distortion(image, camera):
+    # Automatically undistorts image based on the intrinsic and etrinsic matrices
     undistorted_img = cv.undistort(image, camera.intrinsic_matrix, camera.distortion, None, camera.intrinsic_matrix)
     return undistorted_img
+
+
+def stereo_vision(image1, image2, camera1, camera2):
+    stereo = cv.StereoBM_create(numDisparities=16, blockSize=15)
+    disparity = stereo.compute(image1, image2)
+    plt.imshow(disparity,'gray')
+    plt.show()
+
+
+
+
+def feature_finder_sift(image1, image2):
+    # Creates Sift detector
+    sift = cv.SIFT_create()
+
+    # Detect keypoints and compute descriptors
+    keypoints1, descriptors1 = sift.detectAndCompute(image1, None)
+    keypoints2, descriptors2 = sift.detectAndCompute(image2, None)
+
+    # Initialize and use FLANN based matcher
+    FLANN_INDEX_KDTREE = 1
+    index_params = dict(algorithm=FLANN_INDEX_KDTREE, trees=5)
+    search_params = dict(checks=50)
+
+    flann = cv.FlannBasedMatcher(index_params, search_params)
+    matches = flann.knnMatch(descriptors1, descriptors2, k=2)
+
+    # Apply Lowe's ratio test to filter matches7 and 8
+    good_matches = []
+    for m, n in matches:
+        if m.distance < 0.7 * n.distance:
+            good_matches.append(m)
+
+    # Draw matches
+    result_image = cv.drawMatches(image1, keypoints1, image2, keypoints2, good_matches, None)
+    cv.imwrite(os.path.join(r"C:\Users\be1g21\PycharmProjects\pythonProject\Overlap-Part-3" , 'Sift_Example.jpg'), result_image)
+    # Display the result
+    cv.imshow('Feature Matching', result_image)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
 
 
 def transform_points(points, transformation_matrix):
@@ -43,8 +86,12 @@ camera6_distortion_corrected = correct_distortion(images['cam06.jpg'], camera6)
 camera7_distortion_corrected = correct_distortion(images['cam07.jpg'], camera7)
 camera8_distortion_corrected = correct_distortion(images['cam08.jpg'], camera8)
 
+feature_finder_sift(camera7_distortion_corrected, camera8_distortion_corrected)
+stereo_vision(images['cam07.jpg'], images['cam08.jpg'], camera7, camera8)
+
+
 side_by_side = np.concatenate((camera7_distortion_corrected, camera8_distortion_corrected), axis=1)
 cv.imshow('Undistorted Image', side_by_side)
-#cv.imshow('Undistorted Image', camera8_distortion_corrected)
+# cv.imshow('Undistorted Image', camera8_distortion_corrected)
 cv.waitKey(0)  # Wait indefinitely until a key is pressed
 cv.destroyAllWindows()
